481f27b345bad0f2614553fd08df21d5375f9ddd
Jarrod Mosen <jarrod.mosen@me.com>
2014-05-01 16:40:32 +1200

    PHP short tags to standard for compat, shorten lengthy lines

--- fuel/application/views/_install.php
+++ fuel/application/views/_install.php
@@ -5,22 +5,24 @@
 <head>
 	<meta charset="utf-8">
  	<title>Welcome to FUEL CMS</title>
-	<link href='http://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+	<link href="http://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 	<?php echo css('main')?>
-	
+
 </head>
 <body>
 	<div class="page">
 		<div class="wrapper">
 			<header class="page_header">
-				<div class="logo"><object type="image/svg+xml" width="160" height="145" data="<?=img_path('_template_icons.svg#fuel') ?>"></object></div>
+				<div class="logo">
+					<object type="image/svg+xml" width="160" height="145" data="<?php echo img_path('_template_icons.svg#fuel') ?>"></object>
+				</div>
 				<h1>Welcome to Fuel CMS</h1>
-				<h2>Version <?=FUEL_VERSION?></h2>
+				<h2>Version <?php echo FUEL_VERSION?></h2>
 			</header>
 			<section>
 				<header>
 					<div class="icon_block">
-						<object type="image/svg+xml" width="74" height="68" data="<?=img_path('_template_icons.svg#clipboard') ?>"></object>
+						<object type="image/svg+xml" width="74" height="68" data="<?php echo img_path('_template_icons.svg#clipboard') ?>"></object>
 					</div>
 					<div class="content_block">
 						<h3>Getting Started</h3>
@@ -33,9 +35,15 @@
 						</div>
 						<div class="content_block">
 							<h4>Change the Apache .htaccess file</h4>
-							<p>Change the Apache .htaccess found at the root of FUEL CMS's installation folder to the proper RewriteBase directory. The default is your web server's root directory (e.g "/"), but if you have FUEL CMS installed in a sub folder, you will need to add the path to line 5, e.g. <strong>RewriteBase /mysub/folders/</strong>.</p>
-							<p>In some server environments, you may need to add a "?" after index.php in the .htaccess like so: <code>RewriteRule .* index.php?/$0 [L]</code></p>
-							<p class="callout"><strong>NOTE:</strong> This is the only step needed if you want to use FUEL <em>without</em> the CMS.</p>
+							<p>Change the Apache .htaccess found at the root of FUEL CMS's installation folder
+								to the proper RewriteBase directory. The default is your web server's root directory
+								(e.g "/"), but if you have FUEL CMS installed in a sub folder, you will need to
+								add the path to line 5, e.g. <strong>RewriteBase /mysub/folders/</strong>.</p>
+
+							<p>In some server environments, you may need to add a "?" after index.php in
+								the .htaccess like so: <code>RewriteRule .* index.php?/$0 [L]</code></p>
+							<p class="callout"><strong>NOTE:</strong> This is the only step needed
+								if you want to use FUEL <em>without</em> the CMS.</p>
 						</div>
 					</li>
 					<li>
@@ -44,7 +52,12 @@
 						</div>
 						<div class="content_block">
 							<h4>Install the database</h4>
-							<p>Install the FUEL CMS database by first creating the database in MySQL and then importing the <strong>fuel/install/fuel_schema.sql</strong> file. After creating the database, change the database configuration found in <strong>fuel/application/config/database.php</strong> to include your hostname (e.g. localhost), username, password and the database to match the new database you created.</p>
+							<p>Install the FUEL CMS database by first creating the database in MySQL and
+								then importing the <strong>fuel/install/fuel_schema.sql</strong> file.
+								After creating the database, change the database configuration found in
+								<strong>fuel/application/config/database.php</strong> to include your
+								hostname (e.g. localhost), username, password and the database to match
+								the new database you created.</p>
 						</div>
 					</li>
 					<li>
@@ -55,17 +68,17 @@
 							<h4>Make folders writable</h4>
 							<p>Make the following folders writable:</p>
 							<ul class="writable">
-								<li class="<?=(is_really_writable(APPPATH.'cache/')) ? 'success' : 'error'; ?>">
-									<strong><?=APPPATH.'cache/'?></strong><br><span>(folder for holding cache files)</span>
+								<li class="<?php echo (is_really_writable(APPPATH.'cache/')) ? 'success' : 'error'; ?>">
+									<strong><?php echo APPPATH.'cache/'?></strong><br><span>(folder for holding cache files)</span>
 								</li>
-								<li class="<?=(is_really_writable(APPPATH.'cache/dwoo/')) ? 'success' : 'error'; ?>">
-									<strong><?=APPPATH.'cache/dwoo/'?></strong><br><span>(folder for holding Dwoo cache files)</span>
+								<li class="<?php echo (is_really_writable(APPPATH.'cache/dwoo/')) ? 'success' : 'error'; ?>">
+									<strong><?php echo APPPATH.'cache/dwoo/'?></strong><br><span>(folder for holding Dwoo cache files)</span>
 								</li>
-								<li class="<?=(is_really_writable(APPPATH.'cache/dwoo/compiled')) ? 'success' : 'error'; ?>">
-									<strong><?=APPPATH.'cache/dwoo/compiled'?></strong><br><span>(for writing Dwoo compiled template files)</span>
+								<li class="<?php echo (is_really_writable(APPPATH.'cache/dwoo/compiled')) ? 'success' : 'error'; ?>">
+									<strong><?php echo APPPATH.'cache/dwoo/compiled'?></strong><br><span>(for writing Dwoo compiled template files)</span>
 								</li>
-								<li class="<?=(is_really_writable(assets_server_path('', 'images'))) ? 'success' : 'error'; ?>">
-									<strong><?=WEB_ROOT.'assets/images'?></strong><br><span>(for managing image assets in the CMS)</span>
+								<li class="<?php echo (is_really_writable(assets_server_path('', 'images'))) ? 'success' : 'error'; ?>">
+									<strong><?php echo WEB_ROOT.'assets/images'?></strong><br><span>(for managing image assets in the CMS)</span>
 								</li>
 							</ul>
 						</div>
@@ -96,7 +109,7 @@
 						</div>
 					</li>
 					<?php endif; ?>
-					
+
 				</ol>
 				<div>
 					<div class="icon_block"></div>
@@ -104,7 +117,7 @@
 						<h4>That's it!</h4>
 
 						<p>To access the FUEL admin, go to:<br/>
-						<a href="<?=site_url('fuel')?>"><?=site_url('fuel')?></a><br>
+						<a href="<?php echo site_url('fuel')?>"><?php echo site_url('fuel')?></a><br>
 						User name: <strong>admin</strong><br/>
 						Password: <strong>admin</strong> (you can and should change this password and admin user information after logging in)</p>
 					</div>
@@ -114,7 +127,7 @@
 			<section>
 				<header>
 					<div class="icon_block">
-						<div class="logo"><object type="image/svg+xml" width="74" height="68" data="<?=img_path('_template_icons.svg#signpost') ?>"></object></div>
+						<div class="logo"><object type="image/svg+xml" width="74" height="68" data="<?php echo img_path('_template_icons.svg#signpost') ?>"></object></div>
 					</div>
 					<div class="content_block">
 						<h3>What's Next?</h3>
@@ -123,16 +136,16 @@
 				<ol>
 					<li>
 						<div class="icon_block">
-							<div class="circle"><object type="image/svg+xml" width="30" height="27" data="<?=img_path('_template_icons.svg#map') ?>"></object></div>
+							<div class="circle"><object type="image/svg+xml" width="30" height="27" data="<?php echo img_path('_template_icons.svg#map') ?>"></object></div>
 						</div>
 						<div class="content_block">
-							<a class="cta" href="http://docs.getfuelcms.com" target="_blank" style="margin-top: 20px;">User Guide <object type="image/svg+xml" width="30" height="27" data="<?=img_path('_template_icons.svg#rightarrow') ?>"></object></a>
+							<a class="cta" href="http://docs.getfuelcms.com" target="_blank" style="margin-top: 20px;">User Guide <object type="image/svg+xml" width="30" height="27" data="<?php echo img_path('_template_icons.svg#rightarrow') ?>"></object></a>
 							<h4>Visit the <a href="http://docs.getfuelcms.com" target="_blank">1.0 user guide</a> online</h4>
 						</div>
 					</li>
 					<li>
 						<div class="icon_block">
-							<div class="circle"><object type="image/svg+xml" width="40" height="37" data="<?=img_path('_template_icons.svg#github') ?>"></object></div>
+							<div class="circle"><object type="image/svg+xml" width="40" height="37" data="<?php echo img_path('_template_icons.svg#github') ?>"></object></div>
 						</div>
 						<div class="content_block">
 							<h4>Get rolling</h4>
@@ -168,7 +181,10 @@
 				<li class="last"><a href="http://getfuelcms.com/blog" target="_blank">Blog</a></li>
 			</ul>
 		</nav>
-		<p class="colophon">FUEL CMS is developed with love by <a href="http://thedaylightstudio.com" target="_blank">Daylight Studio</a> <object type="image/svg+xml" width="25" height="25" data="<?=img_path('_template_icons.svg#daylight') ?>"></object> &copy; <?php echo date("Y"); ?> Run for Daylight LLC, All Rights Reserved.</p>
+		<p class="colophon">FUEL CMS is developed with love by
+			<a href="http://thedaylightstudio.com" target="_blank">Daylight Studio</a>
+			<object type="image/svg+xml" width="25" height="25" data="<?php echo img_path('_template_icons.svg#daylight')?>"></object>
+			&copy; <?php echo date("Y")?> Run for Daylight LLC, All Rights Reserved.</p>
 	</footer>
 </div>
 </body>
